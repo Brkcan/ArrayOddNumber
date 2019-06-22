9e4d22ed4bdd98bf3fc5e4b1de2db46467b7bd89
Burak <36509701+Brkcan@users.noreply.github.com>
2019-06-23 02:10:44 +0300

    Update ArrayOddNumber.java

--- ArrayOddNumber.java
+++ ArrayOddNumber.java
@@ -1,12 +1,7 @@
-package trendyolTest;
-
 public class ArrayOddNumber {
-
 	public int solution() {
 		int[] A = { 9, 3, 9, 3, 9, 7, 9 };
-
 		int son = 0;
-
 		boolean correct = false;
 		for (int i = 0; i < A.length; i++) {
 			if (A[i] == 0) {
@@ -21,23 +16,12 @@ public class ArrayOddNumber {
 					break;
 				}
 				j++;
-				
 			}
 			if (correct == true) {
 				son = A[i];
 			}
 		}
 		return son;
-
-		// return default
-
-		/**
-		 * for (int i = 0; i < A.length; i++) { if (A[i] == 0) continue; boolean f =
-		 * false; for (int j = i + 1; j < A.length; j++) { if (A[j] == 0) continue; if
-		 * (A[i] == A[j]) { A[i] = A[j] = 0; f = true; } } if (f == false) return A[i];
-		 * } return 0;
-		 */
-
 	}
 
 	public static void main(String[] args) {
